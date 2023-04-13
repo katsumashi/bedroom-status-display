d29eea1f8758fccdd2d677cf281da264a6cf7a52
katsumashi <littlebuglet999@gmail.com>
2023-04-13 17:56:28 +1000

    improved clock?

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function clock() {
     var datetime = new Date();
-    var currentTime = datetime.getHours() + ":" + datetime.getMinutes() + ":" + datetime.getSeconds();
-    document.getElementById('date-time').innerHTML=currentTime;
+    var hours = datetime.getHours();
+    var ampm = "";
+    if (datetime.getHours() >= 13) { hours -= 12; ampm = "pm"; } else { ampm = "am"; }
+    var minutes = datetime.getMinutes()
+    if (datetime.getMinutes() <= 9) { minutes = "0" + minutes; }
+    var seconds = datetime.getSeconds() 
+    if (datetime.getSeconds() <= 9) { seconds = "0" + seconds; }
 
-    setInterval(clock, 1000);
+    var currentTime = hours + ":" + minutes + ":" + seconds + " " + ampm;
+    dateTimeElement = document.getElementById('date-time').innerHTML=currentTime;
+
+    clearTimeout();
+    setTimeout(clock, 1000);
 }
 
 clock();
