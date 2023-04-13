3a4ca2169415bda6e3fb790f71a6709d4dd00c8c
katsumashi <littlebuglet999@gmail.com>
2023-04-13 21:01:32 +1000

    weather?

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const weatherurl = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
+
 function clock() {
     var datetime = new Date();
     var hours = datetime.getHours();
@@ -15,4 +17,19 @@ function clock() {
     setTimeout(clock, 1000);
 }
 
+function weatherget() {
+    fetch(weatherurl)
+    .then(weatherres => weatherres.json())
+    .then(out => {
+        console.log(JSON.stringify(out))
+        var weatherdisplay = out.current_weather.temperature + "°C";
+        weatherelement = document.getElementById("weatherCanberra").innerHTML=weatherdisplay;
+    })
+    .catch(err => { throw err });
+    
+    clearTimeout();
+    setTimeout(clock, 10000);
+}
+
 clock();
+weatherget();
